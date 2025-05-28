38a1829a3a9563e89171f45d7e2ce9c882148767
finmc417 <mckillopfintan@gmail.com>
2025-05-28 11:10:06 +0000

    Separate wordcount into stats

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from stats import wordcount
 def get_book_text(filepath):
     with open(filepath) as f:
         fileContents = f.read()
         return fileContents
-def wordcount(text):
-    words = text.split()
-    return len(words)
 def main():
     filepath = "books/frankenstein.txt"
-    print(f"{wordcount((get_book_text(filepath)))} words found in the document")
+    wordnum = get_book_text(filepath)
+    print(f"{wordcount(wordnum)} words found in the document")
 main()
